92b7ccb1abdfccc150fc45272eb754f9d31b5c6c
Vinson Chuong <vinsonchuong@gmail.com>
2016-10-29 20:42:03 -0700

    Try using setsid directly

--- src/index.js
+++ src/index.js
@@ -106,7 +106,7 @@ export default class {
   }
 
   async start(waitPattern = /^/) {
-    this.server = this.spawn('sh', ['-c', 'npm start'], {detached: true});
+    this.server = this.spawn('setsid', ['npm', 'start']);
     this.server.forEach((output) => {
       process.stderr.write(output);
     });
@@ -120,7 +120,7 @@ export default class {
   async stop() {
     await new Promise((resolve) => {
       this.server.process.on('close', resolve);
-      process.kill(-this.server.process.pid);
+      this.server.process.kill();
     });
   }
 
