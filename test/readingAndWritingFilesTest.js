5611e8d9dab450d625d042724b9c93c710e05bdf
Vinson Chuong <vinsonchuong@gmail.com>
2017-02-12 14:33:07 -0800

    Read and write existing/non-existing files

--- test/readingAndWritingFilesTest.js
+++ test/readingAndWritingFilesTest.js
@@ -13,13 +13,28 @@ test('reading files in the current directory', async (t) => {
   t.true(licenseContents.includes('The MIT License'))
 })
 
-test('creating files in the current directory', async (t) => {
+test('reading files that do not exist', async (t) => {
+  const missingFile = new File('does not exist')
+  try {
+    await missingFile.read()
+    t.fail()
+  } catch (error) {
+    t.is(error.code, 'ENOENT')
+    t.pass()
+  }
+})
+
+test('creating and modifying files in the current directory', async (t) => {
   const fileName = generateUniqueName()
   const fileContents = 'contents'
 
   const newFile = new File(fileName)
   await newFile.write(fileContents)
   t.is(await newFile.read(), fileContents)
+
+  const newFileContents = 'new contents'
+  await newFile.write(newFileContents)
+  t.is(await newFile.read(), newFileContents)
 
   await newFile.remove()
 })
