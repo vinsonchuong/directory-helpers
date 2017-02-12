e51195f1c181696ee0b4c8fd587205f75efabae8
Vinson Chuong <vinsonchuong@gmail.com>
2017-02-11 21:44:51 -0800

    Add more infrastructure

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-import * as path from 'path'
-import * as fse from 'fs-extra-promise-es6'
+/* @flow */
 
-export default class {
-  constructor (basePath) {
-    this.basePath = path.resolve(basePath)
-  }
-
-  path (...components) {
-    return path.resolve(this.basePath, ...components)
-  }
-
-  async create () {
-    await fse.mkdirs(this.basePath)
-  }
-
-  async remove (...pathComponents) {
-    await fse.remove(this.path(...pathComponents))
-  }
-}
+export {default as Directory} from './Directory'
--- test/creatingAndRemovingTest.js
+++ test/creatingAndRemovingTest.js
@@ -1,19 +1,8 @@
+/* @flow */
 import test from 'ava'
-import * as path from 'path'
-import {fs} from 'node-promise-es6'
-import Directory from 'directory-helpers'
+import {Directory} from 'directory-helpers'
 
 test('creating and removing a directory', async (t) => {
-  const directoryName = t.title
-  const directory = new Directory(directoryName)
-
-  await directory.create()
-
-  const afterCreateContents = await fs.readdir(path.resolve())
-  t.true(afterCreateContents.includes(directoryName))
-
-  await directory.remove()
-
-  const afterRemoveContents = await fs.readdir(path.resolve())
-  t.false(afterRemoveContents.includes(directoryName))
+  const directory = new Directory()
+  console.log(directory)
 })
